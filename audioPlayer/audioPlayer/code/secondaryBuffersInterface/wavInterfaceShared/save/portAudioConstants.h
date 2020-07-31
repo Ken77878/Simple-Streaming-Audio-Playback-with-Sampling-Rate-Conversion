9fe3153cde183e45f0dcc2bf5dc809e301cac5dc
Kenichiro Yonezawa <kenichiro.yonezawa@sweet.ocn.ne.jp>
2020-07-31 12:17:11 +0900

    modified .gitignore.

--- audioPlayer/audioPlayer/code/secondaryBuffersInterface/wavInterfaceShared/save/portAudioConstants.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-#include <cstdint>
-
-//audio
-constexpr unsigned char sampleSize = 2;//16bit
-constexpr uint16_t samplingRate = 44100;
-constexpr uint32_t framesPerBuffer = 32;
--- audioPlayer/audioPlayer/code/secondaryBuffersInterface/wavInterfaceShared/save/secondaryBufferWavBase.cpp
+++
@@ -1,7 +0,0 @@
-#include "secondaryBufferWavBase.h"
-
-#include "checkWav.h"
-namespace SecondaryBuffer {
-WavBase::WavBase(Endian endianFlag) : endianFlag(endianFlag) {}
-
-} // namespace SecondaryBuffer
